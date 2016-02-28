8dbde747d5f2884b6bb3eeeef88e9f8eee5e81b8
Ruslan <rarix@ukr.net>
2016-02-28 22:49:01 +0200

    add verification for age

--- unnamed/src/unnamed/Main.java
+++ unnamed/src/unnamed/Main.java
@@ -10,33 +10,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        List<Person> personList = new ArrayList<Person>();
-        while(true) {
-//        	enter and validate name
-            System.out.println("Input Person name");
-            String inputName = scanner.nextLine();
-            while(inputName == null || inputName.length()<3) {
-            	System.out.println("incorrect name, your name should be more than 3 symbols");
-            	 inputName = scanner.nextLine();
-            }
-//            enter and validate age
-            System.out.println("Input Person age");
-            int inputAge;
-            while (true)
-                try {
-                    inputAge = Integer.parseInt(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException nfe) {
-                    System.out.print("Try again: ");
-                }
-            
-            System.out.println("Input Person email");
-            String inputEmail = scanner.nextLine();
-            personList.add(new Person(inputName,inputAge,inputEmail));
-            for (Person person : personList) {
-              System.out.println(person);
-          }
+		
+		Lohic lohic = new Lohic();
+		lohic.welcome();
+		
+		
+		
+		
 //            if (inputLine == "9") 
 //                break;
 //            String[] personFields = inputLine.split(";");
@@ -48,7 +28,7 @@ public class Main {
 //            } else {
 //                System.out.println("Wodi v zayavlennom formate");
 //            }
-        }
+        
 //        for (Person person : chuvakList) {
 //            System.out.println(person);
 //        }
